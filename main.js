e060188201fe41940c87e421c7ac1f7d721f2cf2
polyanne19022007 <106816541+polyanne19022007@users.noreply.github.com>
2023-10-05 11:07:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -10,3 +10,13 @@
         }
         
     }
+
+    const listaDeteclas = document.querySelectorAll('.tecla');
+
+    //para
+    for(let contador< listaDeteclas.length; contador++){
+
+            cont tecl = listtaDeteclas{contador};
+            cont instrumento = tecla.classlist[1];
+            cont idAudio = '#som_${instrumento'; //template tring 
+    }
